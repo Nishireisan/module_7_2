eb524cb7f93bf8a1bb8005a5bbba1198d59e92c6
Nishireisan <andrey240300@gmail.com>
2024-10-20 00:58:00 +0300

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -1,11 +1,11 @@
 def custom_write(file_name, strings):
-    file = open(file_name, 'a', encoding='utf-8')
+    file = open(file_name, 'w', encoding='utf-8')
     k = 1
     strings_positions = {}
     for i in strings:
         h_tuple = (k, file.tell())
         file.write(f'{i}\n')
-        strings_positions.update({f'{h_tuple}': i})
+        strings_positions.update({h_tuple: i})
         k += 1
     file.seek(0)
     file.close()
